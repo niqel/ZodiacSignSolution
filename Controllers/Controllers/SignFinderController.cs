637604de0a52e04b04cc03f2466c6c324d04c2ef
Niqel <niqel504@hotmail.com>
2021-08-10 02:31:30 -0500

    +Se crean los controladdores y los modelos
    1.- Se crea controladores métodos e interfaces

---
+++ Controllers/Controllers/SignFinderController.cs
@@ -0,0 +1,51 @@
+﻿using Controllers.IControllers;
+using Controllers.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Controllers.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SignFinderController : ControllerBase
+    {
+        private readonly ISignFinder<Sign> signFinder;
+
+        public SignFinderController(ISignFinder<Sign> signFinder)
+        {
+            this.signFinder = signFinder;
+        }
+
+        [HttpGet]
+        public IEnumerable<Sign> GetAll()
+        {
+            var reply = signFinder.GetAllSigns();
+            return reply.Items;
+        }
+
+        [HttpGet ("{id}")]
+        public Sign Get(int id)
+        {
+            var reply = signFinder.GetSignById(id);
+            return reply.Items.FirstOrDefault();
+        }
+
+        [HttpGet("{idElement}")]
+        public IEnumerable<Sign> GetByIdElement(int idElement)
+        {
+            var reply = signFinder.GetSignsByElementId(idElement);
+            return reply.Items;
+        }
+
+        [HttpGet("{idPLanet}")]
+        public IEnumerable<Sign> getByIdPlanet(int idPlanet)
+        {
+            var reply = signFinder.GetSignsByPlanetId(idPlanet);
+            return reply.Items;
+        }
+    }
+}
---
+++ WebApiZodiacSigns/Controllers/SignFinderController.cs
@@ -0,0 +1,51 @@
+﻿using Controllers.IControllers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApiZodiacSigns.Models;
+
+namespace WebApiZodiacSigns.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SignFinderController : ControllerBase
+    {
+        private readonly ISignFinder<Sign> signFinder;
+
+        public SignFinderController(ISignFinder<Sign> signFinder)
+        {
+            this.signFinder = signFinder;
+        }
+
+        [HttpGet]
+        public ReplySign GetAll()
+        {
+            var reply = signFinder.GetAllSigns();
+            return reply;
+        }
+
+        [HttpGet("{id}")]
+        public ReplySign Get(int id)
+        {
+            var reply = signFinder.GetSignById(id);
+            return reply;
+        }
+
+        [HttpGet("{idElement}")]
+        public ReplySign GetByIdElement(int idElement)
+        {
+            var reply = signFinder.GetSignsByElementId(idElement);
+            return reply;
+        }
+
+        [HttpGet("{idPLanet}")]
+        public ReplySign getByIdPlanet(int idPlanet)
+        {
+            var reply = signFinder.GetSignsByPlanetId(idPlanet);
+            return reply;
+        }
+    }
+}
